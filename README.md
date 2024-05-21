95a935d341409f9a1c6c8422f1d318fa90adee71
devinzhong <devinzhong@outlook.com>
2024-05-22 00:30:17 +0800

    feat: `show-default-config` action support

--- README.md
+++ README.md
@@ -12,7 +12,8 @@ helm cluster-diff [ACTION] [NAME] [CHART] [flags]
 
 ### action
 
-- `generate-comparison-file`
+- `generate-comparison-file`: 生成集群当前配置与 chart 配置的对比文件
+- `show-default-config`: 打印默认插件配置，可以自行重定向保存
 
 ### example
 
@@ -34,5 +35,6 @@ helm cluster-diff generate-comparison-file \
 - [x] 实现初版基础对比功能
 - [x] 接入 Helm 插件机制
 - [x] 补充初始化说明
+- [x] 配置文件输出
 - [ ] 中文文档
 - [ ] 英文文档
--- src/main.py
+++ src/main.py
@@ -84,22 +84,30 @@ def diff(chart_path: str,
         yaml.dump_all(cluster_manifests, outfile, allow_unicode=True)
     print(f'生成文件: {os.path.join(output_path, CLUSTER_DUMP_FILENAME)}.')
 
+def print_default_config():
+    """打印默认配置文件，类似 helm show values，可以使用重定向另行保存
+    """
+    with open(os.path.join(BASEDIR, DEFAULT_CONFIG_FILE), 'r', encoding='utf-8') as config_file:
+        print(config_file.read())
+
 if __name__ == '__main__':
     
     with open(os.path.join(BASEDIR, DOC_FILE), 'r') as doc_file:
         doc_content = doc_file.read()
     parser = argparse.ArgumentParser(description=doc_content, formatter_class=argparse.RawTextHelpFormatter)
 
-    parser.add_argument('action', type=str, help='执行的操作，目前支持的操作：generate-comparison-file')
-    parser.add_argument('release_name', type=str, default='', help='Release Name')
-    parser.add_argument('chart', type=str, help='Chart local path or package')
+    parser.add_argument('action', type=str, help='执行的操作，目前支持的操作：generate-comparison-file、show-default-config')
+    parser.add_argument('release_name', nargs='?', type=str, help='Release Name')
+    parser.add_argument('chart', nargs='?', type=str, help='Chart local path or package')
     parser.add_argument('--values', type=str, help='自定义 values 文件路径')
     parser.add_argument('--output-path', type=str, default=os.path.join(CURRENT_DIRECTORY, DEFAULT_OUPUT_DIRNAME), help='输出比对文件的目录')
     parser.add_argument('--config-path', type=str, default=os.path.join(BASEDIR, DEFAULT_CONFIG_FILE), help='自定义脚本配置文件路径')
     parser.add_argument('--dry-run', nargs='?', const=True, type=bool, help='仅模拟运行，不实际变更集群，update_ownership 操作可使用')
     args = parser.parse_args()
-
-    if args.action == 'generate-comparison-file':
+    
+    if args.action == 'show-default-config':
+        print_default_config()
+    elif args.action == 'generate-comparison-file':
         diff(chart_path=args.chart,
             release_name=args.release_name,
             values=args.values,
